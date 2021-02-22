f5e62597084700edf96fc310f934da1a4fe87729
Fedor Tikhonin <fredr7@yandex.ru>
2021-02-22 23:54:49 +0500

    some changes in xaml

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -9,19 +9,6 @@ namespace SudokuSolver
         {
 
 
-            //Сделать заполнение массива рандомными числами и зависимости от выбора уровня сложности - заполнять определенное кол-во клеток
-            //for (int i = 0; i < 9; i++)
-            //{
-            //    for (int j = 0; j < 9; j++)
-            //    {
-            //        arr[i,j] = j;
-            //    }
-            //}
-
-
-            //bool q = Solver.possible(0, 2, 1);
-            //Console.WriteLine(q ? "true" : "false");
-            //Solver.solve();
             Solver.Print();
             Console.WriteLine();
             for (int y = 0; y < 9; y++)
